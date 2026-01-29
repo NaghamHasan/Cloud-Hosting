c8f6ef9922e55ecbc77429c890bc968b4da80f79
Abdulrahman Falyoun <falyoun.abdulrahman@gmail.com>
2026-01-29 17:23:31 +0200

    https://github.com/NaghamHasan/Cloud-Hosting.git

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -12,8 +12,7 @@ const Header = async () => {
 
   return (
     <header
-      className={`fixed top-0 z-10 w-full flex justify-center items-center h-15`}
-    >
+      className={`fixed top-0 z-10 w-full flex justify-center items-center h-15`}>
       <div className="container py-1 black-bg px-6 main-text-color flex w-full justify-between rounded-3xl items-center">
         <div className="font-bold relative text-2xl hidden md:inline-block">
         <Image src={"/cloud hosting-logo (1).png"} alt="bhv" width={"150"} height={"150"} priority className="object-contain"/>
